5702d6dec66853be27ead292583a363fe6b97cf1
Kevin Gabriel Alves de Melo <67153082+kevingabrielmelo@users.noreply.github.com>
2021-09-20 11:32:09 -0300

    Add files via upload

---
+++ handkbook-plane/src/main/java/com/handkbookplane/HandkbookPlaneApplication.java
@@ -0,0 +1,12 @@
+package com.handkbookplane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandkbookPlaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandkbookPlaneApplication.class, args);
+	}
+}
