a9900dc2842f329d6b05f9d2bd94f0f27c332b24
Danvargaa <danielvgh@hotmail.com>
2021-11-06 16:40:03 -0300

    telas dos tracos, blocos, codelist e lep além de algumas correçoes

---
+++ src/main/java/com/handkbookplane/controller/VisualizarBlocoController.java
@@ -0,0 +1,34 @@
+package com.handkbookplane.controller;
+
+import com.handkbookplane.model.Administrador;
+import com.handkbookplane.model.Usuario;
+import com.handkbookplane.repository.AdministradorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Classe responsável por controlar as funções do sistema
+ *
+ * @Author: Luiz Miguel
+ * @Since: 10/10/2021
+ */
+@Controller
+public class VisualizarBlocoController {
+
+    @Autowired
+    AdministradorRepository administradorRepository;
+    /**
+     * Método responsável por dar um get na tela menu Traço
+     * @return ModelAndView
+     */
+    @GetMapping(value = "/visualizarBloco")
+    public ModelAndView telavisualizarBloco() {
+        Administrador administrador = administradorRepository.findByIdAdmin(Usuario.IdUsu);
+
+        ModelAndView mv = new ModelAndView("/bloco/visualizarBloco");
+        mv.addObject("administrador", administrador);
+        return mv;
+    }
+}
