1b641cc629a238348a0880ee3cfc98fbac8cef77
Edryan Maciel <emaciel709@gmail.com>
2021-11-07 19:07:52 -0300

    Cadastrar bloco sem PDF

---
+++ src/main/java/com/handkbookplane/model/BlocoControllerModel.java
@@ -0,0 +1,119 @@
+package com.handkbookplane.model;
+
+import javax.persistence.GeneratedValue;
+import java.io.Serializable;
+
+public class BlocoControllerModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer idBloco;
+    private String nomeBloco;
+    private String secao;
+    private String subsecao;
+    private Integer nbloco;
+    private Integer code;
+    private Integer revisao;
+    private String dataRev;
+    private String remark;
+    private byte[] PDF;
+    private String PDF_string;
+
+
+
+
+    public Integer getIdBloco() {
+        return idBloco;
+    }
+
+    public void setIdBloco(Integer idBloco) {
+        this.idBloco = idBloco;
+    }
+
+    public String getNomeBloco() {
+        return nomeBloco;
+    }
+
+    public void setNomeBloco(String nomeBloco) {
+        this.nomeBloco = nomeBloco;
+    }
+
+    public String getSecao() {
+        return secao;
+    }
+
+    public void setSecao(String secao) {
+        this.secao = secao;
+    }
+
+    public String getSubsecao() {
+        return subsecao;
+    }
+
+    public void setSubsecao(String subsecao) {
+        this.subsecao = subsecao;
+    }
+
+    public Integer getNbloco() {
+        return nbloco;
+    }
+
+    public void setNbloco(Integer nbloco) {
+        this.nbloco = nbloco;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Integer getRevisao() {
+        return revisao;
+    }
+
+    public void setRevisao(Integer revisao) {
+        this.revisao = revisao;
+    }
+
+    public String getDataRev() {
+        return dataRev;
+    }
+
+    public void setDataRev(String dataRev) {
+        this.dataRev = dataRev;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public byte[] getPDF() {
+        return PDF;
+    }
+
+    public void setPDF(byte[] PDF) {
+        this.PDF = PDF;
+    }
+
+    public String getPDF_string() {
+        return PDF_string;
+    }
+
+    public void setPDF_string(String PDF_string) {
+        this.PDF_string = PDF_string;
+    }
+
+
+
+
+
+
+
+}
