3a2caa7ba1904ef47192de33a286747dfb36c8c1
kevingabrielmelo <kevin_gabriel_melo@hotmail.com>
2021-11-07 20:13:57 -0300

    Finalizando cadastro e revisão de blocos

--- src/main/java/com/handkbookplane/controller/EmitirRevisaoController.java
+++ src/main/java/com/handkbookplane/controller/EmitirRevisaoController.java
@@ -1,12 +1,18 @@
 package com.handkbookplane.controller;
 
 import com.handkbookplane.model.Administrador;
+import com.handkbookplane.model.Bloco;
 import com.handkbookplane.model.Usuario;
 import com.handkbookplane.repository.AdministradorRepository;
+import com.handkbookplane.repository.BlocoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
 
 /**
  * Classe responsável por controlar as funções do sistema
@@ -19,6 +25,9 @@ public class EmitirRevisaoController {
 
     @Autowired
     AdministradorRepository administradorRepository;
+
+    @Autowired
+    BlocoRepository blocoRepository;
     /**
      * Método responsável por dar um get na tela menu Traço
      * @return ModelAndView
@@ -28,6 +37,37 @@ public class EmitirRevisaoController {
         Administrador administrador = administradorRepository.findByIdAdmin(Usuario.IdUsu);
 
         ModelAndView mv = new ModelAndView("/bloco/emitirRevisao");
+
+        Iterable<Bloco> bloco = blocoRepository.findAll();
+
+        List<Bloco> blocosTotais = new ArrayList<>();
+
+        for (Bloco blocos : bloco) {
+
+            String pdf = Base64.getEncoder().encodeToString(blocos.getPDF());
+            blocos.setPDF_string(pdf);
+            blocosTotais.add(blocos);
+        }
+
+        mv.addObject("bloco", blocosTotais);
+
+        mv.addObject("administrador", administrador);
+        return mv;
+    }
+
+    @GetMapping(value = "/revisaoBloco/{idBloco}")
+    public ModelAndView telarevisaoBloco(Integer idBLoco) {
+        Administrador administrador = administradorRepository.findByIdAdmin(Usuario.IdUsu);
+
+        ModelAndView mv = new ModelAndView("/bloco/revisaoBloco");
+
+        Bloco bloco = blocoRepository.findByIdBloco(idBLoco);
+
+            String pdf = Base64.getEncoder().encodeToString(bloco.getPDF());
+            bloco.setPDF_string(pdf);
+
+        mv.addObject("bloco", bloco);
+
         mv.addObject("administrador", administrador);
         return mv;
     }
