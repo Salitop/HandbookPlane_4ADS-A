8a2932da137017679bd08b8d41ad000eb95361ab
Edryan Maciel <emaciel709@gmail.com>
2021-11-25 19:38:07 -0300

    Pesquisar bloco da tela do traço

--- src/main/java/com/handkbookplane/controller/CriarTracoController.java
+++ src/main/java/com/handkbookplane/controller/CriarTracoController.java
@@ -1,13 +1,19 @@
 package com.handkbookplane.controller;
 
 import com.handkbookplane.model.Administrador;
+import com.handkbookplane.model.Bloco;
 import com.handkbookplane.model.Usuario;
 import com.handkbookplane.repository.AdministradorRepository;
+import com.handkbookplane.repository.BlocoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
 /**
  * Classe responsável por controlar as funções do sistema
  *
@@ -16,7 +22,8 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class CriarTracoController {
-
+    @Autowired
+    BlocoRepository blocoRepository;
     @Autowired
     AdministradorRepository administradorRepository;
     /**
@@ -24,10 +31,54 @@ public class CriarTracoController {
      * @return ModelAndView
      */
     @GetMapping(value = "/criarTraco")
-    public ModelAndView telacriarTraco() {
+    public ModelAndView telacriarTraco(String name) {
         Administrador administrador = administradorRepository.findByIdAdmin(Usuario.IdUsu);
 
         ModelAndView mv = new ModelAndView("/tracos/criarTraco");
+
+        if (name != null) {
+
+            ArrayList<Bloco> bloco = blocoRepository.findByNomeBloco(name);
+
+            if (bloco.size() != 0) {
+
+                List<Bloco> blocosTotais = new ArrayList<>();
+
+                for (Bloco blocos : bloco) {
+
+                    String pdf = Base64.getEncoder().encodeToString(blocos.getPDF());
+                    blocos.setPDF_string(pdf);
+                    blocosTotais.add(blocos);
+                }
+
+                mv.addObject("bloco", blocosTotais);
+
+                mv.addObject("administrador", administrador);
+
+                return mv;
+            }
+
+            Iterable<Bloco> blocos = blocoRepository.findAll();
+
+            mv.addObject("bloco", blocos);
+
+            mv.addObject("administrador", administrador);
+            return mv;
+        }
+
+        Iterable<Bloco> bloco = blocoRepository.findAll();
+
+        List<Bloco> blocosTotais = new ArrayList<>();
+
+        for (Bloco blocos : bloco) {
+
+            String pdf = Base64.getEncoder().encodeToString(blocos.getPDF());
+            blocos.setPDF_string(pdf);
+            blocosTotais.add(blocos);
+        }
+
+        mv.addObject("bloco", blocosTotais);
+
         mv.addObject("administrador", administrador);
         return mv;
     }
