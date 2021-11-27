85af8abe7ffd3ec3c287b2cf3188b34b2a4e1715
Edryan Maciel <emaciel709@gmail.com>
2021-11-26 21:29:58 -0300

    Listar da tela administrador

--- src/main/java/com/handkbookplane/repository/AdministradorRepository.java
+++ src/main/java/com/handkbookplane/repository/AdministradorRepository.java
@@ -1,8 +1,11 @@
 package com.handkbookplane.repository;
 
 import com.handkbookplane.model.Administrador;
+import com.handkbookplane.model.Bloco;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
 
 /**
  * Interface responsável por comunicar com o banco de dados, utilizando funções básicas como:
@@ -19,4 +22,6 @@ public interface AdministradorRepository extends CrudRepository<Administrador, S
     Administrador findByLogin(String login);
 
     Administrador findByIdAdmin(Integer idAdmin);
+
+    ArrayList<Administrador>  findByNome (String nome);
 }
